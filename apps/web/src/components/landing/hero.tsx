737bc7e5e0a5d0cc65eef91232fe574b14de2928
Maze Winther <mazewinther@gmail.com>
2025-06-29 19:38:30 +0200

    fix: hero height

--- apps/web/src/components/landing/hero.tsx
+++ apps/web/src/components/landing/hero.tsx
@@ -69,7 +69,7 @@ export function Hero({ signupCount }: HeroProps) {
   };
 
   return (
-    <div className="min-h-[calc(100vh-6rem)] supports-[height:100dvh]:min-h-[calc(100dvh-6rem)] flex flex-col justify-between items-center text-center px-4">
+    <div className="min-h-[calc(100vh-4.5rem)] supports-[height:100dvh]:min-h-[calc(100dvh-4.5rem)] flex flex-col justify-between items-center text-center px-4">
       <Image
         className="absolute top-0 left-0 -z-50 size-full object-cover"
         src="/landing-page-bg.png"
