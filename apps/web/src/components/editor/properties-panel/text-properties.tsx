62f2317a4c498373dbe2234dc50ef4b6ecadaad9
Maze Winther <mazewinther@gmail.com>
2025-07-10 22:40:01 +0200

    background

--- apps/web/src/components/editor/properties-panel/text-properties.tsx
+++ apps/web/src/components/editor/properties-panel/text-properties.tsx
@@ -25,7 +25,7 @@ export function TextProperties({
       <Textarea
         placeholder="Name"
         defaultValue={element.content}
-        className="min-h-[4.5rem] resize-none bg-background/75"
+        className="min-h-[4.5rem] resize-none bg-background/50"
         onChange={(e) =>
           updateTextElement(trackId, element.id, { content: e.target.value })
         }
