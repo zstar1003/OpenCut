3a5a249f7ac04484b5d9572cf4592032ba93f5ac
Anwarul Islam <anwaarulislaam@gmail.com>
2025-07-12 23:52:17 +0600

    chore: remove GripVertical as it is an unused import

--- apps/web/src/components/ui/resizable.tsx
+++ apps/web/src/components/ui/resizable.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { GripVertical } from "lucide-react";
 import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "../../lib/utils";
@@ -36,4 +35,4 @@ const ResizableHandle = ({
   />
 );
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
+export { ResizableHandle, ResizablePanel, ResizablePanelGroup };
