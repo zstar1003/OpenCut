ce26c189be2d7a375bc23e9d21d843352fb59556
Maze Winther <mazewinther@gmail.com>
2025-07-18 11:54:32 +0200

    fix: z-index of playhead

--- apps/web/src/components/editor/timeline-playhead.tsx
+++ apps/web/src/components/editor/timeline-playhead.tsx
@@ -66,7 +66,7 @@ export function TimelinePlayhead({
   return (
     <div
       ref={playheadRef}
-      className="absolute pointer-events-auto z-[100]"
+      className="absolute pointer-events-auto z-[150]"
       style={{
         left: `${leftPosition}px`,
         top: 0,
