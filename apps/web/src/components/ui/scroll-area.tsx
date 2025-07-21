49a7af2b346eca3400b9a48e75d2381412bdee3e
Maze Winther <mazewinther@gmail.com>
2025-07-21 21:42:44 +0200

    Merge pull request [#395](https://github.com/mazeincoding/AppCut/issues/395)

--- apps/web/src/components/ui/scroll-area.tsx
+++ apps/web/src/components/ui/scroll-area.tsx
@@ -7,17 +7,22 @@ import { cn } from "../../lib/utils";
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
+    type?: "auto" | "always" | "scroll" | "hover";
+    showHorizontalScrollbar?: boolean;
+  }
+>(({ className, children, type, showHorizontalScrollbar, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
+    type={type}
     {...props}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
+    {showHorizontalScrollbar && <ScrollBar orientation="horizontal" />}
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ));
