bce91a7b1ed9259fc3f2bacaad849a2efa2393c6
Maze Winther <mazewinther@gmail.com>
2025-08-05 15:21:26 +0200

    nvm one more thing

--- apps/web/src/components/editor-header.tsx
+++ apps/web/src/components/editor-header.tsx
@@ -181,7 +181,7 @@ function ExportButton() {
       <div className="flex items-center gap-1.5 bg-linear-270 from-[#2567EC] to-[#37B6F7] rounded-[0.8rem] px-4 py-1 relative shadow-[0_1px_3px_0px_rgba(0,0,0,0.45)]">
         <TransitionUpIcon className="z-50" />
         <span className="text-[0.875rem] z-50">Export</span>
-        <div className="absolute w-full h-full left-0 top-0 bg-linear-to-t from-white/0 to-white/65 z-10 rounded-[0.8rem] flex items-center justify-center">
+        <div className="absolute w-full h-full left-0 top-0 bg-linear-to-t from-white/0 to-white/50 z-10 rounded-[0.8rem] flex items-center justify-center">
           <div className="absolute w-[calc(100%-4px)] h-[calc(100%-4px)] top-[0.12rem] bg-linear-270 from-[#2567EC] to-[#37B6F7] z-50 rounded-lg"></div>
         </div>
       </div>
