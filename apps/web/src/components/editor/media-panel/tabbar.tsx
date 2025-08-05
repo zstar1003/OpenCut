718de64ecd267c4795848cd163da660ae9249f0e
Maze Winther <mazewinther@gmail.com>
2025-08-05 08:26:39 +0200

    refactor: remove unused imports

--- apps/web/src/components/editor/media-panel/tabbar.tsx
+++ apps/web/src/components/editor/media-panel/tabbar.tsx
@@ -2,12 +2,11 @@
 
 import { cn } from "@/lib/utils";
 import { Tab, tabs, useMediaPanelStore } from "./store";
-import { Button } from "@/components/ui/button";
-import { ChevronRight, ChevronLeft } from "lucide-react";
-import { useRef, useState, useEffect } from "react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function TabBar() {
   const { activeTab, setActiveTab } = useMediaPanelStore();
@@ -21,7 +20,9 @@ export function TabBar() {
             <div
               className={cn(
                 "flex z-[100] flex-col gap-0.5 items-center cursor-pointer",
-                activeTab === tabKey ? "text-primary !opacity-100" : "text-muted-foreground"
+                activeTab === tabKey
+                  ? "text-primary !opacity-100"
+                  : "text-muted-foreground"
               )}
               onClick={() => setActiveTab(tabKey)}
               key={tabKey}
@@ -30,8 +31,15 @@ export function TabBar() {
                 <TooltipTrigger asChild>
                   <tab.icon className="size-[1.1rem]! opacity-100 hover:opacity-75" />
                 </TooltipTrigger>
-                <TooltipContent side="right" align="center" variant="sidebar" sideOffset={8}>
-                  <div className="dark:text-base-gray-950 text-black text-sm font-medium leading-none dark:text-white">{tab.label}</div>
+                <TooltipContent
+                  side="right"
+                  align="center"
+                  variant="sidebar"
+                  sideOffset={8}
+                >
+                  <div className="dark:text-base-gray-950 text-black text-sm font-medium leading-none dark:text-white">
+                    {tab.label}
+                  </div>
                 </TooltipContent>
               </Tooltip>
             </div>
